8fa1a4a459c6969cefe9b02eb82a53ebcb7aaebf
alexgl9 <105857929+alexgl9@users.noreply.github.com>
2025-03-12 22:13:09 +0100

    DeepSeek Zero lite

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from telegram.error import Conflict
 USER_DATA_FILE = "user_data.pkl"
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-DEEPSEEK_MODEL = "deepseek/deepseek-r1-lite-preview"  # Deepseek r1
+DEEPSEEK_MODEL = "deepseek/deepseek-r1-zero:free"  # Deepseek r1
 
 # Завантаження даних користувачів
 try:
